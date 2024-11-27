1044253f211ebba519049e731f6e4f101f55d89c
João Távora <capitaomorte@archlinux2022.linuxvmimages.local>
2024-11-27 06:42:08 -0600

    Not sure I missed writing plain C

---
+++ 01-simplethread/main.c
@@ -0,0 +1,37 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <pthread.h>
+
+#define NUM_THREADS 64
+
+typedef struct
+{
+    int tid;
+} tparams_t;
+
+pthread_t threads[NUM_THREADS];
+tparams_t params[NUM_THREADS];
+
+void* count_locally(void* arg) {
+  tparams_t* a = (tparams_t*)arg;
+  int sum = 0;
+  int tid = a->tid;
+  for (int i = 1; i<=tid; ++i) sum += i;
+  printf("Thread[%d] sum[1..%d]=%d\n", tid, tid, sum);
+  return NULL;
+}
+
+int main(int argc, char* argv[]) {
+
+  for (int i = 0; i<NUM_THREADS; ++i) {
+    params[i].tid = i;
+    if (pthread_create(&threads[i], NULL, &count_locally, &params[i]) != 0) {
+      perror("pthread_create");
+      exit(-1);
+    }
+  }
+
+  for (int i = 0; i<NUM_THREADS; ++i) pthread_join(threads[i], NULL);
+  printf("Done!\n");
+  return 0;
+}
