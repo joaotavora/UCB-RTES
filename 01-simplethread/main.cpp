7c8e94c7a76806f988c53e3af47f8d61014448e0
João Távora <joaotavora@gmail.com>
2024-12-01 01:19:46 +0000

    Admit defeat

--- 01-simplethread/main.cpp
+++ 01-simplethread/main.cpp
@@ -1,26 +1,22 @@
 #include <cstdio>
 #include <array>
-
-#include "xpto/pthread.hpp"
+#include <thread>
 
 static constexpr size_t num_threads{64};
   // namespace xpto
 
 int main(int argc, char* argv[]) {
-  {
-    auto fn = [](int tid) {
+  std::array<std::thread, num_threads> threads;
+
+  // threads.reserve(num_threads);
+  for (auto i = 0u; i < num_threads; ++i) {
+    threads.at(i) = std::thread{[](int tid) {
       size_t sum{};
       for (int i = 1; i <= tid; ++i) sum += i;
       printf("Thread[%d] sum[1..%d]=%lu\n", tid, tid, sum);
-    };
-    using thread_t = xpto::thread<decltype(fn)>;
-    std::array<thread_t, num_threads> threads;
-
-    // threads.reserve(num_threads);
-    for (auto i = 0u; i < num_threads; ++i) {
-      threads.at(i) = xpto::thread{fn, i};
-    }
+    }, i};
   }
+  for (auto& t : threads) t.join();
 
   printf("Done!\n");
   return 0;
