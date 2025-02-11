c8a258c09dbfd0c0da42d62494f04f9997f1b8f1
João Távora <joaotavora@gmail.com>
2025-02-11 17:52:08 +0000

    Ongoing

--- 07-hardcoded-sequencer/main.cpp
+++ 07-hardcoded-sequencer/main.cpp
@@ -1,4 +1,5 @@
 #include "xpto/thread.hpp"
+#include "xpto/rtes-misc.hpp"
 #include <chrono>
 #include <print>
 
@@ -23,10 +24,22 @@ int FIB_TEST(unsigned int seqCnt, unsigned int iterCnt) {
 }
 
 int main() {
-  std::chrono::high_resolution_clock rt;
-  auto t1 = rt.now();
-  FIB_TEST(70, 10000);
-  auto elapsed = rt.now() - t1;
-  auto ms = std::chrono::duration_cast<std::chrono::milliseconds>(elapsed);
-  std::println("This work took {}", ms);
+  xpto::install_rt_scheduler();
+
+  using attributes = xpto::thread::attributes;
+  auto rt_max_prio = sched_get_priority_max(SCHED_FIFO);
+  attributes attr;
+
+  auto i  = 0;
+  xpto::thread t1{{
+    .policy = attributes::FIFO,
+    .prio = rt_max_prio - i - 1,
+  }, []() {
+    std::chrono::high_resolution_clock rt;
+    auto t1 = rt.now();
+    FIB_TEST(230, 10000); // around 10 us on a RP5
+    auto elapsed = rt.now() - t1;
+    auto us = std::chrono::duration_cast<std::chrono::microseconds>(elapsed);
+    std::println("This work took {}", us);
+  }};
 }
