4577bade4d6f8f2c0a14985646c510f9058313d0
João Távora <joaotavora@gmail.com>
2025-02-15 00:31:56 +0000

    priority queue timer list

--- 08-sequencer-generic/generic-sequencer.cpp
+++ 08-sequencer-generic/generic-sequencer.cpp
@@ -1,10 +1,10 @@
 #include <chrono>
 #include <cstddef>
-#include <numeric>
 #include <print>
+#include <queue>
 #include <ratio>
 #include <string>
-#include <map>
+#include <thread>
 
 #include "xpto/semaphore.hpp"
 #include "xpto/syslog.hpp"
@@ -13,90 +13,87 @@
 using freq_t = size_t;
 
 struct service {
+  using duration_t = std::chrono::duration<size_t, std::nano>;
+
   std::string name;
   freq_t frequency;
-  using duration_t = std::chrono::duration<size_t, std::nano>;
-  auto constexpr period() const { return duration_t{std::giga::num / frequency}; }
+  duration_t phase{};
+
   size_t cycles{};
-  xpto::thread t{};
   xpto::sem sem{name + "sem"};
+  xpto::thread t{};
   bool abort{};
+
+  duration_t constexpr period() const {
+    return duration_t{std::giga::num / frequency};
+  }
 };
 
-struct step {
-  service::duration_t wait{};
-  std::vector<service*> services{};
-};
+using seconds_float_t = std::chrono::duration<double>;
+using milliseconds_float_t = std::chrono::duration<double, std::milli>;
 
-using sequence_t = std::vector<step>;
-
-constexpr auto as_seconds(auto duration) {
-  return std::chrono::duration_cast<std::chrono::duration<double>>(duration);
-}
-
-sequence_t sequence(std::span<service> in) {
-  sequence_t retval;
-  std::map<service::duration_t, std::vector<service*>> map;
-
-  using namespace std::chrono_literals;
-  auto hyperperiod = std::accumulate(
-      in.begin(), in.end(), service::duration_t{1},  //
-      [](service::duration_t acc,
-         auto&& s) -> std::chrono::duration<size_t, std::nano> {
-        auto period = s.period();
-        std::println("Period of {} is {}", s.name, s.period());
-        auto ccount = std::lcm(acc.count(), s.period().count());
-        return service::duration_t{ccount};
-      });
-
-  std::println("Hyperperiod is {}", as_seconds(hyperperiod));
-
-  for (auto&& s : in) {
-    auto period = s.period();
-    for (service::duration_t i{0}; i < hyperperiod; i+= period) {
-      map[i].push_back(&s);
-    }
-  }
-
-  std::vector<std::pair<service::duration_t, std::vector<service*>>> hmmm;
-
-  service::duration_t last{0};
-  for (auto&& x : map) {
-    retval.emplace_back(x.first - last, x.second);
-    last = x.first;
-  }
-  return retval;
-}
-
-
-
+using queue_element_t = std::pair<service::duration_t, service*>;
+using queue_t = std::priority_queue<
+    queue_element_t, std::vector<queue_element_t>,
+    std::greater<queue_element_t>>;
 
 const xpto::syslogger logger{""};
 
 int main() {
-
   auto services = std::array<service, 3>{
-    service{"bla", 30},
-    service{"bla", 3},
-    service{"bla", 1},
+    service{"t1", 10},
+    service{"t2", 3},
+    service{"t3", 1},
   };
 
-  auto const myseq = sequence(services);
+  queue_t queue;
+  service::duration_t elapsed{0};
 
-  for (auto&& x : myseq) {
-    std::print("{} -> [", as_seconds(x.wait));
-    auto sep = "";
-    for (auto&& y : x.services) {
-      std::print("{}{}", sep, y->name);
-      sep = ", ";
-    }
-    std::println("]");
+  for (auto&& x : services) {
+    x.t = xpto::thread({}, [&]() {
+      while (!x.abort) {
+        x.sem.wait();
+        if (x.abort) break;
+        ++x.cycles;
+        logger.debug(
+            "start: {} @ {}", x.name,
+            seconds_float_t(elapsed));
+      }
+      logger.debug("done: {} ", x.name);
+    });
+    queue.emplace(x.phase, &x);
   }
-  
+
+  logger.debug("queue has {} elements", queue.size());
+
+
   std::chrono::steady_clock stc;
+  auto t1 = stc.now();
 
-  // services.emplace_back(std::make_unique<service>("s1", 10));
-  // services.emplace_back(std::make_unique<service>("s2", 50));
+  using namespace std::chrono_literals;
+  do { // NOLINT
+    auto& top = queue.top();
+    auto& x = *top.second;
+    auto rem = top.first - elapsed;
+    if (rem > 0s) {
+      std::this_thread::sleep_for(rem);
+      elapsed = top.first;
+    }
+    logger.debug("signalling {} @ {}", x.name, seconds_float_t{elapsed});
+    x.sem.post();
+    queue.pop();
+    queue.emplace(elapsed + x.period(), &x);
+  } while (elapsed < 3s);
 
-  
+  auto clock_elapsed = stc.now() - t1;
+
+  logger.debug("elapsed: {} clock_elapsed: {} diff {}",
+    elapsed,
+    clock_elapsed,
+      milliseconds_float_t{clock_elapsed-elapsed});
+
+  for (auto&& x : services) {
+    x.abort = true;
+    x.sem.post();
+  }
 }
