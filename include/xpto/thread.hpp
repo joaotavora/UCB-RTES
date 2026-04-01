04705d35f9bb14cb0d7fbfdd3b3d06663405b92e
João Távora <joaotavora@gmail.com>
2026-04-01 11:26:03 +0100

    include/xpto/thread.hpp no longer uses Iglberger-style type erasure
    Use std::move_only_function instead, is good enough

--- include/xpto/thread.hpp
+++ include/xpto/thread.hpp
@@ -3,6 +3,7 @@
 #include <pthread.h>
 #include <sched.h>
 
+#include <functional>
 #include <memory>
 #include <optional>
 #include <set>
@@ -30,96 +31,92 @@ class thread {
   };
 
  private:
-  struct thread_interface {
-    thread_interface() = default;
-    thread_interface(const thread_interface &) = delete;
-    thread_interface(thread_interface &&) = default;
-    thread_interface &operator=(const thread_interface &) = delete;
-    thread_interface &operator=(thread_interface &&) = default;
-    virtual ~thread_interface() = default;
-  };
-
-  template <typename F, typename... Args>
-  struct thread_model : thread_interface {
-    pthread_t tid_{};
-    F f_;
-    std::tuple<Args...> args_;
-    thread_model(const thread_model &) = delete;
-    thread_model(thread_model &&) = default;
-    thread_model &operator=(const thread_model &) = delete;
-    thread_model &operator=(thread_model &&) = default;
-
-    thread_model(const attributes &attrs, F f, Args... args)
-        : f_{std::move(f)}, args_{std::move(args)...} {
-      // Create the thread with specified attributes,
-      pthread_attr_t pattrs{};
-      xpto::or_lose(pthread_attr_init(&pattrs));
-      AUTO(pthread_attr_destroy(&pattrs));
-
-      cpu_set_t cpuset;
-      CPU_ZERO(&cpuset);
-      if (attrs.affinity.size()) {
-        for (auto x : attrs.affinity) CPU_SET(x, &cpuset);
-        xpto::or_lose(
-            pthread_attr_setaffinity_np(&pattrs, sizeof(cpuset), &cpuset));
-      }
-      if (attrs.stack_size) {
-        xpto::or_lose(
-            pthread_attr_setstacksize(&pattrs, attrs.stack_size.value()));
-      }
-
-      if (attrs.policy) {
-        pthread_attr_setinheritsched(&pattrs, PTHREAD_EXPLICIT_SCHED);
-        pthread_attr_setschedpolicy(&pattrs, attrs.policy.value());
-
-        struct sched_param sparam{};
-        int prio{};
-        if (attrs.prio)
-          sparam.sched_priority = attrs.prio.value();
-        else
-          sparam.sched_priority = sched_get_priority_max(attrs.policy.value());
-
-        // Not sure if this one is needed
-        xpto::or_lose(
-            sched_setscheduler(getpid(), attrs.policy.value(), &sparam));
-
-        pthread_attr_setschedparam(&pattrs, &sparam);
-      }
-
-      auto lambda = [](void *arg) -> void * {
-        auto self = static_cast<thread_model *>(arg);
-        std::apply(self->f_, self->args_);
-        return nullptr;
-      };
-
-      ZCALL_OR_LOSE(pthread_create(&tid_, &pattrs, lambda, this));
-      if (attrs.name.size()) {
-        ZCALL_OR_LOSE(pthread_setname_np(tid_, attrs.name.c_str()));
-      }
-    }
-    ~thread_model() override {
-      // FIXME: check joinable threads (errcode)
-      // FIXME: make don't do this, add join method instead
-      if (tid_) pthread_join(tid_, nullptr);
-    }
-  };
-
-  std::unique_ptr<thread_interface> pimpl_;
+  std::move_only_function<void()> f_;
+  pthread_t tid_{};
+  attributes attrs_{};
 
  public:
-  template <typename F, class... Args>
-    requires std::invocable<F, Args...>
-  explicit thread(F &&f, Args &&...args)
-      : pimpl_{std::make_unique<thread_model<F, Args...>>(
-            attributes{}, std::forward<F>(f), std::forward<Args>(args)...)} {}
+  template <typename F>
+    requires std::invocable<F>
+  explicit thread(F&& f) : thread{{}, std::forward<F>(f)} {}
 
-  template <typename F, class... Args>
-    requires std::invocable<F, Args...>
-  explicit thread(const attributes &attr, F &&f, Args &&...args)
-      : pimpl_{std::make_unique<thread_model<F, Args...>>(
-            attr, std::forward<F>(f), std::forward<Args>(args)...)} {}
+  friend void swap(thread& a, thread& b) noexcept {
+    std::swap(a.f_, b.f_);
+    std::swap(a.tid_, b.tid_);
+    std::swap(a.attrs_, b.attrs_);
+  }
 
   thread() = default;
+  thread(const thread&) = delete;
+  thread& operator=(const thread&) = delete;
+  thread(thread&& o) noexcept
+      : f_{std::move(o.f_)}, tid_{o.tid_}, attrs_{std::move(o.attrs_)} {
+    o.tid_ = 0;
+  }
+  thread& operator=(thread&& rhs) noexcept {
+    if (&rhs != this) {
+      thread tmp{std::move(rhs)};
+      swap(*this, tmp);
+    }
+    return *this;
+  }
+
+  template <typename F>
+    requires std::invocable<F>
+  explicit thread(attributes a, F&& f)
+      : attrs_{std::move(a)}, f_{std::forward<F>(f)} {
+    // Create the thread with specified attributes,
+    pthread_attr_t pattrs{};
+    xpto::or_lose(pthread_attr_init(&pattrs));
+    AUTO(pthread_attr_destroy(&pattrs));
+
+    cpu_set_t cpuset;
+    CPU_ZERO(&cpuset);
+    if (attrs_.affinity.size()) {
+      for (auto x : attrs_.affinity) CPU_SET(x, &cpuset);
+      xpto::or_lose(
+          pthread_attr_setaffinity_np(&pattrs, sizeof(cpuset), &cpuset));
+    }
+    if (attrs_.stack_size) {
+      xpto::or_lose(
+          pthread_attr_setstacksize(&pattrs, attrs_.stack_size.value()));
+    }
+
+    if (attrs_.policy) {
+      pthread_attr_setinheritsched(&pattrs, PTHREAD_EXPLICIT_SCHED);
+      pthread_attr_setschedpolicy(&pattrs, attrs_.policy.value());
+
+      struct sched_param sparam{};
+      int prio{};
+      if (attrs_.prio)
+        sparam.sched_priority = attrs_.prio.value();
+      else
+        sparam.sched_priority = sched_get_priority_max(attrs_.policy.value());
+
+      // Not sure if this one is needed
+      xpto::or_lose(
+          sched_setscheduler(getpid(), attrs_.policy.value(), &sparam));
+
+      pthread_attr_setschedparam(&pattrs, &sparam);
+    }
+
+    auto lambda = [](void* arg) -> void* {
+      auto self = static_cast<thread*>(arg);
+      std::invoke(self->f_);
+      return nullptr;
+    };
+
+    ZCALL_OR_LOSE(pthread_create(&tid_, &pattrs, lambda, this));
+    if (attrs_.name.size()) {
+      ZCALL_OR_LOSE(pthread_setname_np(tid_, attrs_.name.c_str()));
+    }
+  }
+
+  ~thread() {
+    // FIXME: check joinable threads (errcode)
+    // FIXME: make don't do this, add join method instead
+    if (tid_) pthread_join(tid_, nullptr);
+  }
 };
 
 }  // namespace xpto
